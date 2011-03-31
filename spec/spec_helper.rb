4f233e216e4a15be64c361e08a23dd8627d08769
Tony Arcieri <tony@medioh.com>
2011-03-30 22:59:33 -0600

    The default spec_helper is a bit silly and ugly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,9 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.dirname(__FILE__)
+
 require 'rspec'
 require 'celluloid'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-  
-end
