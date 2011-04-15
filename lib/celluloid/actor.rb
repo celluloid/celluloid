b512a4081608d824f7a36e944a6578632c4ab03c
Tony Arcieri <tony.arcieri@gmail.com>
2011-04-15 12:08:45 -0600

    Factor internal Celluloid::Actor methods into their own module

--- lib/celluloid/actor.rb
+++ lib/celluloid/actor.rb
@@ -5,6 +5,7 @@ module Celluloid
   module Actor
     attr_reader :celluloid_mailbox
     
+    # Methods added to classes which include Celluloid::Actor
     module ClassMethods  
       def spawn(*args, &block)
         actor = allocate
@@ -14,39 +15,43 @@ module Celluloid
       end
     end
     
-    # Actor-specific initialization
-    def __initialize_actor(*args, &block)
-      @celluloid_mailbox = Mailbox.new
+    # Internal methods not intended as part of the public API
+    module InternalMethods
+      # Actor-specific initialization
+      def __initialize_actor(*args, &block)
+        @celluloid_mailbox = Mailbox.new
       
-      # Call the object's normal initialize method
-      initialize(*args, &block)
+        # Call the object's normal initialize method
+        initialize(*args, &block)
       
-      Thread.new { __run_actor }
-    end
-    
-    # Run the actor
-    def __run_actor
-      Thread.current[:celluloid_mailbox] = @celluloid_mailbox
-      __process_messages
-    rescue Exception => ex
-      __handle_crash(ex)
-    end
-    
-    # Process incoming messages
-    def __process_messages
-      while true # instead of loop, for speed!
-        call = @celluloid_mailbox.receive
-        call.dispatch(self)
+        Thread.new { __run_actor }
       end
-    end
     
-    # Handle any exceptions that occur within a running actor
-    def __handle_crash(ex)
-      puts "*** #{self.class} CRASH: #{ex.class}: #{ex.to_s}\n#{ex.backtrace.join("\n")}"
+      # Run the actor
+      def __run_actor
+        Thread.current[:celluloid_mailbox] = @celluloid_mailbox
+        __process_messages
+      rescue Exception => ex
+        __handle_crash(ex)
+      end
+    
+      # Process incoming messages
+      def __process_messages
+        while true # instead of loop, for speed!
+          call = @celluloid_mailbox.receive
+          call.dispatch(self)
+        end
+      end
+    
+      # Handle any exceptions that occur within a running actor
+      def __handle_crash(ex)
+        puts "*** #{self.class} CRASH: #{ex.class}: #{ex.to_s}\n#{ex.backtrace.join("\n")}"
+      end
     end
   
     def self.included(klass)
-      klass.extend(ClassMethods)
+      klass.extend ClassMethods
+      klass.send :include, InternalMethods
     end
   end
 end
