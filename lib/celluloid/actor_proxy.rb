3c6686fba94a8fe9fedf4de3956acc196a1c1534
Tony Arcieri <tony.arcieri@gmail.com>
2011-05-01 14:27:45 -0600

    Proxy #send, #respond_to?, and #methods to actors

--- lib/celluloid/actor_proxy.rb
+++ lib/celluloid/actor_proxy.rb
@@ -3,6 +3,7 @@ module Celluloid
   # dispatches calls and casts to normal Ruby objects which are running inside
   # of their own threads.
   class ActorProxy
+    # FIXME: not nearly enough methods are delegated here
     attr_reader :celluloid_mailbox
     
     def initialize(actor)
@@ -10,15 +11,34 @@ module Celluloid
       @celluloid_mailbox = actor.celluloid_mailbox
     end
     
+    def send(meth, *args, &block)
+      __call :send, meth, *args, &block
+    end
+    
+    def respond_to?(meth)
+      __call :respond_to?, meth
+    end
+    
+    def methods(include_ancestors = true)
+      __call :methods, include_ancestors
+    end
+    
+    # method_missing black magic to call bang predicate methods asynchronously
     def method_missing(meth, *args, &block)
-      our_mailbox = Thread.current.celluloid_mailbox
-      
       # bang methods are async calls
       if meth.to_s.match(/!$/) 
         unbanged_meth = meth.to_s.sub(/!$/, '')
+        our_mailbox = Thread.current.celluloid_mailbox
         @celluloid_mailbox << AsyncCall.new(our_mailbox, unbanged_meth, args, block)
         return # casts are async and return immediately
       end
+      
+      __call(meth, *args, &block)
+    end
+    
+    # Make a synchronous call to the actor we're proxying to
+    def __call(meth, *args, &block)
+      our_mailbox = Thread.current.celluloid_mailbox
       
       @celluloid_mailbox << SyncCall.new(our_mailbox, meth, args, block)
       response = our_mailbox.receive
