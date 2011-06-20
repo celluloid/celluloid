cd235d69511094cc4a41f552fd2e6e6e77a34e7d
Tony Arcieri <tony.arcieri@gmail.com>
2011-06-19 19:00:26 -0600

    Roflscale the mailbox

--- lib/celluloid/mailbox.rb
+++ lib/celluloid/mailbox.rb
@@ -38,34 +38,30 @@ module Celluloid
     end
     
     # Receive a message from the Mailbox
-    def receive(&block)
+    def receive
       message = nil
       
       @lock.synchronize do
         raise MailboxError, "attempted to receive from a dead mailbox" if @dead
         
         begin
-          message = locate(&block)
+          if block_given?
+            index = @messages.index do |msg|
+              yield(msg) || msg.is_a?(Celluloid::SystemEvent)
+            end
+
+            message = @messages.slice!(index, 1).first if index
+          else
+            message = @messages.shift
+          end
+          
           raise message if message.is_a?(Celluloid::SystemEvent)
           
           @condition.wait(@lock) unless message
-        end while message.nil?
+        end until message
       end
         
       message
-    end
-    
-    # Locate and remove a message from the mailbox which matches the given filter
-    def locate
-      if block_given?
-        index = @messages.index do |msg|
-          yield(msg) || msg.is_a?(Celluloid::SystemEvent)
-        end
-      
-        @messages.slice!(index, 1).first if index
-      else
-        @messages.shift
-      end
     end
     
     # Cleanup any IO objects this Mailbox may be using
