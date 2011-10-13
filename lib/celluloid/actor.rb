294a9938cc44f48579adb6bc442a154d2a8bdd9e
Tony Arcieri <tony.arcieri@gmail.com>
2011-10-13 10:25:11 -0700

    Friendlier error message for JRuby

--- lib/celluloid/actor.rb
+++ lib/celluloid/actor.rb
@@ -3,8 +3,10 @@ require 'thread'
 begin
   require 'fiber'
 rescue LoadError => ex
-  # If we're on Rubinius, we can still work in 1.8 mode
-  if defined? Rubinius
+  if defined? JRUBY_VERSION
+    raise LoadError, "Celluloid requires JRuby 1.9 mode. Please pass the --1.9 flag or set JRUBY_OPTS=--1.9"
+  elsif defined? Rubinius
+    # If we're on Rubinius, we can still work in 1.8 mode
     Fiber = Rubinius::Fiber
   else
     raise ex
