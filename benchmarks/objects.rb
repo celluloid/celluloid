2cf894e35c82974057681e2a58f956f52a138572
Tony Arcieri <tony.arcieri@gmail.com>
2011-10-18 23:09:56 -0700

    Benchmark creation speed of ephemeral objects

--- benchmarks/objects.rb
+++ benchmarks/objects.rb
@@ -32,13 +32,28 @@ end
 
 puts "objects_per_second:"
 
-sequential_creation = measure(100000) { RegularObject.new }
-concurrent_creation = measure(1000)   { ConcurrentObject.new }
+objs = []
+sequential_creation = measure(100000) { objs << RegularObject.new }
+objs.clear
+
+objs = []
+concurrent_creation = measure(1000)   { objs << ConcurrentObject.new }
+objs.each { |obj| obj.terminate! }
+objs.clear
 
 puts "  sequential: #{format sequential_creation}"
 puts "  concurrent: #{format concurrent_creation}"
 
 puts "  delta: #{format sequential_creation / concurrent_creation }"
+
+#
+# CREATION OF SHORT LIVED OBJECTS
+#
+
+puts "epehemeral_objects_per_second:"
+
+ephemeral_creation = measure(5000) { ConcurrentObject.new.terminate! }
+puts "  concurrent: #{format ephemeral_creation}"
 
 #
 # METHOD CALLS
