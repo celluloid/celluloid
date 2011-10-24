77134ff3f00b529cf100fe1e52658f6a9d7e45e4
Tony Arcieri <tony.arcieri@gmail.com>
2011-10-23 21:38:02 -0700

    Make Celluloid::IO::Mailbox a subclass of Celluloid::Mailbox
    I hate factoring things this way but Celluloid::Mailbox has functionality that
    Celluloid::IO::Mailbox duplicates. This sort of subclass relationship is a bit
    ugly, but it's better than duplicating functionality.

--- lib/celluloid/io/mailbox.rb
+++ lib/celluloid/io/mailbox.rb
@@ -3,12 +3,8 @@ require 'thread'
 module Celluloid
   module IO
     # An alternative implementation of Celluloid::Mailbox using Wakers
-    class Mailbox
-      include Enumerable
-
-      def initialize
-        @messages = []
-        @lock  = Mutex.new
+    class Mailbox < Celluloid::Mailbox
+      def initialize_signaling
         @waker = Waker.new
       end
 
@@ -43,9 +39,8 @@ module Celluloid
 
         begin
           @waker.wait
-          message = locate(&block)
-          raise message if message.is_a?(SystemEvent)
-        end while message.nil?
+          message = next_message(&block)
+        end until message
 
         message
       rescue DeadWakerError
@@ -53,47 +48,10 @@ module Celluloid
         raise MailboxError, "mailbox shutdown called during receive"
       end
 
-      # Locate and remove a message from the mailbox which matches the given filter
-      def locate
-        @lock.synchronize do
-          if block_given?
-            index = @messages.index do |msg|
-              yield(msg) || msg.is_a?(SystemEvent)
-            end
-
-            @messages.slice!(index, 1).first if index
-          else
-            @messages.shift
-          end
-        end
-      end
-
       # Cleanup any IO objects this Mailbox may be using
       def shutdown
-        messages = nil
-
-        @lock.synchronize do
-          @waker.cleanup
-          messages = @messages
-          @messages = []
-        end
-
-        messages.each { |msg| msg.cleanup if msg.respond_to? :cleanup }
-      end
-
-      # Cast to an array
-      def to_a
-        @lock.synchronize { @messages.dup }
-      end
-
-      # Iterate through the mailbox
-      def each(&block)
-        to_a.each(&block)
-      end
-
-      # Inspect the contents of the Mailbox
-      def inspect
-        "#<Celluloid::Mailbox[#{map { |m| m.inspect }.join(', ')}]>"
+        @waker.cleanup
+        super
       end
     end
   end
--- lib/celluloid/mailbox.rb
+++ lib/celluloid/mailbox.rb
@@ -2,30 +2,34 @@ require 'thread'
 
 module Celluloid
   class MailboxError < StandardError; end # you can't message the dead
-  
+
   # Actors communicate with asynchronous messages. Messages are buffered in
   # Mailboxes until Actors can act upon them.
   class Mailbox
     include Enumerable
-    
+
     def initialize
       @messages = []
       @lock  = Mutex.new
-      @condition = ConditionVariable.new
       @dead = false
+      initialize_signaling
     end
-    
+
+    def initialize_signaling
+      @condition = ConditionVariable.new
+    end
+
     # Add a message to the Mailbox
     def <<(message)
       @lock.synchronize do
         raise MailboxError, "dead recipient" if @dead
-        
+
         @messages << message
         @condition.signal
       end
       nil
     end
-    
+
     # Add a high-priority system event to the Mailbox
     def system_event(event)
       @lock.synchronize do
@@ -36,58 +40,65 @@ module Celluloid
       end
       nil
     end
-    
+
     # Receive a message from the Mailbox
-    def receive
+    def receive(&block)
       message = nil
-      
+
       @lock.synchronize do
         raise MailboxError, "attempted to receive from a dead mailbox" if @dead
-        
-        begin
-          if block_given?
-            index = @messages.index do |msg|
-              yield(msg) || msg.is_a?(Celluloid::SystemEvent)
-            end
 
-            message = @messages.slice!(index, 1).first if index
-          else
-            message = @messages.shift
-          end
-          
-          raise message if message.is_a?(Celluloid::SystemEvent)
-          
+        begin
+          message = next_message(&block)
           @condition.wait(@lock) unless message
         end until message
       end
-        
+
       message
     end
-    
+
+    # Retrieve the next message in the mailbox
+    def next_message
+      message = nil
+
+      if block_given?
+        index = @messages.index do |msg|
+          yield(msg) || msg.is_a?(Celluloid::SystemEvent)
+        end
+
+        message = @messages.slice!(index, 1).first if index
+      else
+        message = @messages.shift
+      end
+
+      raise message if message.is_a?(Celluloid::SystemEvent)
+      message
+    end
+
     # Shut down this mailbox and clean up its contents
     def shutdown
       messages = nil
-      
+
       @lock.synchronize do
         messages = @messages
         @messages = []
         @dead = true
       end
-      
-      messages.each { |msg| msg.cleanup if msg.respond_to? :cleanup }      
+
+      messages.each { |msg| msg.cleanup if msg.respond_to? :cleanup }
       true
     end
-    
+
     # Cast to an array
     def to_a
       @lock.synchronize { @messages.dup }
     end
-    
+
     # Iterate through the mailbox
     def each(&block)
       to_a.each(&block)
     end
-    
+
     # Inspect the contents of the Mailbox
     def inspect
       "#<Celluloid::Mailbox:#{object_id} [#{map { |m| m.inspect }.join(', ')}]>"
