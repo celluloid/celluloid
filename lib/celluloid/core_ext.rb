30e5f2e5d592aeb7a6d5705f9acb94204ecb833a
Tony Arcieri <tony.arcieri@gmail.com>
2011-10-24 23:57:55 -0700

    If I'm going to write a core extension, it really shouldn't be super crazysauce

--- lib/celluloid/core_ext.rb
+++ lib/celluloid/core_ext.rb
@@ -1,17 +1,13 @@
 # Monkeypatch Thread to allow lazy access to its Celluloid::Mailbox
 class Thread
-  # Slightly insane global lock for lazy-initializing the mailbox of ANY thread
-  @@mailbox_lock = Mutex.new
-
   # Retrieve the current mailbox or lazily initialize it
   def mailbox
-    # I'm aware this is super ghetto but it's only here for threads created
-    # outside of Celluloid itself. Celluloid is good about initializing the
-    # mailbox thread local itself non-lazily.
     self[:mailbox] || begin
-      @@mailbox_lock.synchronize do
-        self[:mailbox] ||= Celluloid::Mailbox.new
+      if Thread.current != self
+        raise "attempt to access an uninitialized mailbox"
       end
+
+      self[:mailbox] = Celluloid::Mailbox.new
     end
   end
 end
