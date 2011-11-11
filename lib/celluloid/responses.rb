11585e91cec48b34d232d25fda59af41a40e5749
Tony Arcieri <tony.arcieri@gmail.com>
2011-11-10 17:52:47 -0800

    Factor response handling into the response objects

--- lib/celluloid/responses.rb
+++ lib/celluloid/responses.rb
@@ -12,5 +12,15 @@ module Celluloid
   class SuccessResponse < Response; end
   
   # Call was aborted due to caller error
-  class ErrorResponse < Response; end
+  class ErrorResponse < Response
+    def value
+      if super.is_a? AbortError
+        # Aborts are caused by caller error, so ensure they capture the
+        # caller's backtrace instead of the receiver's
+        raise super.cause.class.new(super.cause.message)
+      else
+        raise super
+      end
+    end
+  end
 end
