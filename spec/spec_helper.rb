9ca1ac24d1f3fe584492024e6f73667ba7ba150b
Tony Arcieri <tony.arcieri@gmail.com>
2011-11-29 16:39:56 -0800

    Turn on logger output by default
    Crash output is needed to properly debug Celluloid. It does make running
    the test suite pretty noisy though. There's crashes going on on Travis
    that I can't debug right now because I don't have the log.
    
    Perhaps the test runner could output the crashes to a logfile instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,6 @@ Bundler.setup
 require 'celluloid'
 
 # Squelch the logger (comment out this line if you want it on for debugging)
-Celluloid.logger = nil
+#Celluloid.logger = nil
 
 Dir['./spec/support/*.rb'].map {|f| require f }
