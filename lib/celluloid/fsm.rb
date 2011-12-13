0c8d7ac806494b30ab590185c4c99dc20c429788
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-13 14:59:36 -0800

    Automatically transition to the default state after the FSM is initialized

--- lib/celluloid/fsm.rb
+++ lib/celluloid/fsm.rb
@@ -2,6 +2,8 @@ module Celluloid
   # Turn concurrent objects into finite state machines
   # Inspired by Erlang's gen_fsm. See http://www.erlang.org/doc/man/gen_fsm.html
   module FSM
+    DEFAULT_STATE = :default # Default state name unless one is explicitly set
+
     # Included hook to extend class methods
     def self.included(klass)
       klass.send :include, Celluloid
@@ -9,6 +11,30 @@ module Celluloid
     end
 
     module ClassMethods
+      # Ensure FSMs transition into the default state after they're initialized
+      def new
+        fsm = super
+        fsm.transition default_state
+        fsm
+      end
+
+      # Ensure FSMs transition into the default state after they're initialized
+      def new_link
+        fsm = super
+        fsm.transition default_state
+        fsm
+      end
+
+      # Obtain or set the default state
+      # Passing a state name sets the default state
+      def default_state(new_default = nil)
+        if new_default
+          @default_state = new_default.to_sym
+        else
+          defined?(@default_state) ? @default_state : DEFAULT_STATE
+        end
+      end
+
       # Obtain the valid states for this FSM
       def states
         @states ||= {}
@@ -18,10 +44,6 @@ module Celluloid
       def state(name, &block)
         name = name.to_sym
         states[name] = State.new(name, &block)
-      end
-
-      def default_state
-        states ? states.first : :default
       end
     end
 
@@ -34,7 +56,15 @@ module Celluloid
     # Transition to another state
     def transition(state_name)
       new_state = self.class.states[state_name.to_sym]
-      raise ArgumentError, "invalid state for #{self.class}: #{state_name}" unless new_state
+
+      unless new_state
+        if state_name == DEFAULT_STATE
+          @state = state_name
+          return
+        end
+
+        raise ArgumentError, "invalid state for #{self.class}: #{state_name}" unless new_state
+      end
 
       @state = new_state.name
       new_state.call(self)
