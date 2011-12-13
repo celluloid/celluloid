b86d04c08b325a8c357b6f56a7d59f95afc94a0e
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-12 23:14:47 -0800

    Some minor cleanups

--- lib/celluloid/zmq/mailbox.rb
+++ lib/celluloid/zmq/mailbox.rb
@@ -3,9 +3,10 @@ module Celluloid
     # A Celluloid mailbox for Actors that wait on 0MQ sockets
     class Mailbox < Celluloid::IO::Mailbox
       def initialize
+        # More APIs and less monkeypatching would be useful here
         @messages = []
-        @lock  = Mutex.new
-        @waker = Celluloid::ZMQ::Waker.new
+        @lock    = Mutex.new
+        @waker   = Waker.new
         @reactor = Reactor.new(@waker)
       end
     end
