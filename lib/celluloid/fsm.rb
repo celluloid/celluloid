591c02ca50e56e3424bff2a8d7e00176e8468b10
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-16 17:45:41 -0800

    Hurrrrk, this shouldn't be

--- lib/celluloid/fsm.rb
+++ lib/celluloid/fsm.rb
@@ -8,7 +8,6 @@ module Celluloid
     def self.included(klass)
       klass.send :include, Celluloid
       klass.send :extend,  ClassMethods
-      klass.send :attr_reader, :state
     end
 
     module ClassMethods
