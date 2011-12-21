0115196fcf7ca6affabab78dafd9aca93758488a
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-20 17:11:57 -0800

    Fix the tests to use the new Celluloid::Future API

--- spec/celluloid/future_spec.rb
+++ spec/celluloid/future_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe Celluloid::Future do
   it "creates future objects that can be retrieved later" do
-    future = Celluloid::Future() { 40 + 2 }
+    future = Celluloid::Future.new { 40 + 2 }
     future.value.should == 42
   end
   
   it "passes arguments to future blocks" do
-    future = Celluloid::Future(40) { |n| n + 2 }
+    future = Celluloid::Future.new(40) { |n| n + 2 }
     future.value.should == 42
   end
   
   it "reraises exceptions that occur when the value is retrieved" do
     class ExampleError < StandardError; end
     
-    future = Celluloid::Future() { raise ExampleError, "oh noes crash!" }
+    future = Celluloid::Future.new { raise ExampleError, "oh noes crash!" }
     expect { future.value }.to raise_exception(ExampleError)
   end
 end
