58f8a9bb4a76489f69670a24d8b42d57bba3b10f
Tony Arcieri <tony.arcieri@gmail.com>
2012-01-02 14:08:48 -0800

    require 'celluloid' in simple pmap example

--- examples/simple_pmap.rb
+++ examples/simple_pmap.rb
@@ -1,3 +1,8 @@
+#!/usr/bin/env ruby
+
+$:.push File.expand_path('../../lib', __FILE__)
+require 'celluloid'
+
 module Enumerable
   # Simple parallel map using Celluloid::Futures
   def pmap(&block)
