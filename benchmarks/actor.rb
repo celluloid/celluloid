04b16f8fe771bf6c1322005834575bf4d91f4374
Tony Arcieri <tony.arcieri@gmail.com>
2012-01-19 19:32:10 -0800

    Benchmark end-to-end message transmission rate

--- benchmarks/actor.rb
+++ benchmarks/actor.rb
@@ -13,9 +13,23 @@ end
 example_actor = ExampleActor.new
 mailbox = Celluloid::Mailbox.new
 
+latch_in, latch_out = Queue.new, Queue.new
+latch = Thread.new do
+  while true
+    n = latch_in.pop
+    for i in 0..n; mailbox.receive; end
+    latch_out << :done
+  end
+end
+
 Benchmark.ips do |ips|
   ips.report("spawn")       { ExampleActor.new.terminate }
   ips.report("calls")       { example_actor.example_method }
   ips.report("async calls") { example_actor.example_method! }
-  ips.report("messages")    { mailbox << :message }
+  
+  ips.report("messages") do |n|
+    latch_in << n
+    for i in 0..n; mailbox << :message; end
+    latch_out.pop
+  end
 end
