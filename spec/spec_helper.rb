aa890e09a8abb6a9d452849b0a3d33442e629d88
Ben Langfeld <ben@langfeld.me>
2012-01-31 16:13:42 +0000

    Allow focus-filtering spec examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,8 @@ require 'celluloid'
 #Celluloid.logger = nil
 
 Dir['./spec/support/*.rb'].map {|f| require f }
+
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+end
