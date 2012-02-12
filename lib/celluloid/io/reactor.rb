4496594e232ee9b3890195a60663d08f013fb8a0
Tony Arcieri <tony.arcieri@gmail.com>
2012-02-11 17:56:39 -0800

    Fix IO type conversion bug

--- lib/celluloid/io/reactor.rb
+++ lib/celluloid/io/reactor.rb
@@ -29,7 +29,7 @@ module Celluloid
       # Wait for the given IO operation to complete
       def wait(io, set)
         # zomg ugly type conversion :(
-        unless io.is_a?(IO)
+        unless io.is_a?(::IO)
           if IO.respond_to? :try_convert
             io = IO.try_convert(io)
           elsif io.respond_to? :to_io
