9fadac44df9419745bcbac21eb88930a94b67868
Tony Arcieri <tony.arcieri@gmail.com>
2012-02-11 17:28:16 -0800

    Handle failed IO.try_convert

--- lib/celluloid/io/reactor.rb
+++ lib/celluloid/io/reactor.rb
@@ -25,7 +25,7 @@ module Celluloid
       def wait_writeable(io)
         wait io, :w
       end
-      
+
       # Wait for the given IO operation to complete
       def wait(io, set)
         # zomg ugly type conversion :(
@@ -34,21 +34,22 @@ module Celluloid
             io = IO.try_convert(io)
           elsif io.respond_to? :to_io
             io = io.to_io
-          else raise TypeError, "can't convert #{io.class} into IO"
           end
+
+          raise TypeError, "can't convert #{io.class} into IO" unless io.is_a? IO
         end
-        
+
         monitor = @selector.register(io, set)
         monitor.value = Task.current
         Task.suspend :iowait
       end
-      
+
       # Run the reactor, waiting for events or wakeup signal
       def run_once(timeout = nil)
         @selector.select_each(timeout) do |monitor|
           task = monitor.value
           # Only resume task if it is still running. If task is dead
-          # (e.g. it finished prematurely by itself) there is no need 
+          # (e.g. it finished prematurely by itself) there is no need
           # to resume it.
           task.resume if task.running?
           @selector.deregister(monitor.io)
