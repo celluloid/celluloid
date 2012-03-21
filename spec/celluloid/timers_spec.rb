172a6e8d393259cf6a4661bc3582798d3d45579b
Ben Langfeld <ben@langfeld.me>
2012-03-21 12:24:31 +0000

    Refactor out an unnecessary spec subject duplication

--- spec/celluloid/timers_spec.rb
+++ spec/celluloid/timers_spec.rb
@@ -1,17 +1,13 @@
 require 'spec_helper'
 
 describe Celluloid::Timers do
-  before :each do
-    @timers = Celluloid::Timers.new
-  end
-
   it "sleeps until the next timer" do
     interval = 0.1
     started_at = Time.now
 
     fired = false
-    @timers.add(interval) { fired = true }
-    @timers.wait
+    subject.add(interval) { fired = true }
+    subject.wait
 
     fired.should be_true
     (Time.now - started_at).should be_within(Celluloid::Timer::QUANTUM).of interval
@@ -20,8 +16,8 @@ describe Celluloid::Timers do
   it "it calculates the interval until the next timer should fire" do
     interval = 0.1
 
-    @timers.add(interval)
-    @timers.wait_interval.should be_within(Celluloid::Timer::QUANTUM).of interval
+    subject.add(interval)
+    subject.wait_interval.should be_within(Celluloid::Timer::QUANTUM).of interval
   end
 
   it "fires timers in the correct order" do
@@ -29,12 +25,12 @@ describe Celluloid::Timers do
 
     Q = Celluloid::Timer::QUANTUM
 
-    @timers.add(Q * 2) { result << :two }
-    @timers.add(Q * 3) { result << :three }
-    @timers.add(Q * 1) { result << :one }
+    subject.add(Q * 2) { result << :two }
+    subject.add(Q * 3) { result << :three }
+    subject.add(Q * 1) { result << :one }
 
     sleep 0.03 + Celluloid::Timer::QUANTUM
-    @timers.fire
+    subject.fire
 
     result.should == [:one, :two, :three]
   end
