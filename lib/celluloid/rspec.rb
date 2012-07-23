1fb3a5c79c31f038788b5d4be01726eeb502cbfa
Ben Langfeld <ben@langfeld.me>
2012-07-23 12:43:53 +0300

    [BUGFIX] Dependencies' examples need the example actor class

--- lib/celluloid/rspec.rb
+++ lib/celluloid/rspec.rb
@@ -1,2 +1,3 @@
+require File.expand_path('../../../spec/support/example_actor_class', __FILE__)
 require File.expand_path('../../../spec/support/actor_examples', __FILE__)
 require File.expand_path('../../../spec/support/mailbox_examples', __FILE__)
