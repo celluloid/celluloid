fdb2b24ab49ac967b60c5d39dec62663b34f1015
Tony Arcieri <tony.arcieri@gmail.com>
2012-08-02 00:24:11 -0700

    Put TIMER_QUANTUM into the shared specs

--- lib/celluloid/rspec.rb
+++ lib/celluloid/rspec.rb
@@ -1,3 +1,6 @@
 require File.expand_path('../../../spec/support/example_actor_class', __FILE__)
 require File.expand_path('../../../spec/support/actor_examples', __FILE__)
 require File.expand_path('../../../spec/support/mailbox_examples', __FILE__)
+
+# Timer accuracy enforced by the tests (50ms)
+TIMER_QUANTUM = 0.05
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'celluloid'
+require 'celluloid/rspec'
 
 # Squelch the logger (comment out this line if you want it on for debugging)
 Celluloid.logger = nil
@@ -11,6 +12,3 @@ RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 end
-
-# Timer accuracy enforced by the tests (50ms)
-TIMER_QUANTUM = 0.05
