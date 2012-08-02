2c356487feab57504ac3072e65cb1efac6303697
Tony Arcieri <tony.arcieri@gmail.com>
2012-08-01 21:02:19 -0700

    Add some debug logging for Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 require 'celluloid'
 
 # Squelch the logger (comment out this line if you want it on for debugging)
-#Celluloid.logger = nil
+Celluloid.logger = nil
 
 Dir['./spec/support/*.rb'].map {|f| require f }
 
