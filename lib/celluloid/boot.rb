0dd2c23e32139e68f297e448b3aa6721f5c96d3b
Grant Rodgers <grantr@gmail.com>
2012-09-14 15:27:21 -0700

    use IncidentLogger as Celluloid.logger
    IncidentLogger needs to be started after the fanout actor.
    
    IncidentLogger should also be one of the last actors to shut down,
    otherwise shutdown messages are lost.

--- lib/celluloid/boot.rb
+++ lib/celluloid/boot.rb
@@ -1,9 +1,11 @@
 # Things to run after Celluloid is fully loaded
 
 # Configure default systemwide settings
-Celluloid.logger     = Logger.new STDERR
 Celluloid.task_class = Celluloid::TaskFiber
 
 # Launch the notifications fanout actor
 # FIXME: We should set up the supervision hierarchy here
 Celluloid::Notifications::Fanout.supervise_as :notifications_fanout
+
+Celluloid.logger = Celluloid::IncidentLogger.new
+Celluloid::IncidentReporter.supervise_as :default_incident_reporter, STDERR
