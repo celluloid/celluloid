f90eab8505b1980ca3b533e31060947bb14a522b
Grant Rodgers <grantr@gmail.com>
2012-09-14 15:18:13 -0700

    use a Logger to write incident events
    Includes a custom formatter that uses the event time instead of the time
    given by Logger, which is likely to be inaccurate as most events
    actually happened in the past.
    
    The actual format of the log output is the same as the default.

--- lib/celluloid/logging/incident_reporter.rb
+++ lib/celluloid/logging/incident_reporter.rb
@@ -1,18 +1,29 @@
+require 'logger'
 module Celluloid
   # Subscribes to log incident topics to report on them.
   class IncidentReporter
     include Celluloid
     include Celluloid::Notifications
 
+    # get the time from the event
+    class Formatter < ::Logger::Formatter
+      def call(severity, time, progname, msg)
+        super(severity, msg.timestamp, progname, msg.message)
+      end
+    end
+
     def initialize
       subscribe(/log\.incident/, :report)
+      @logger = ::Logger.new(STDERR)
+      @logger.formatter = Formatter.new
     end
 
     def report(topic, incident)
+
       puts "INCIDENT"
       puts "===================="
       incident.events.each do |event|
-        puts event.message
+        @logger.add(event.severity, event, event.progname)
       end
       puts "===================="
     end
