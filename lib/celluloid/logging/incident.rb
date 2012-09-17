6b3fc7bad9b1f517c6a3ef7da20020c4b3f514ae
Grant Rodgers <grantr@gmail.com>
2012-09-17 13:14:16 -0700

    allow triggering_event to be nil

--- lib/celluloid/logging/incident.rb
+++ lib/celluloid/logging/incident.rb
@@ -4,7 +4,7 @@ module Celluloid
     attr_accessor :pid
     attr_accessor :events, :triggering_event
 
-    def initialize(events, triggering_event)
+    def initialize(events, triggering_event=nil)
       @events = events
       @triggering_event = triggering_event
       @pid = $$
