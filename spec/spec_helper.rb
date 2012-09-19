0c32968cb8a510d6f15db7fde0a8b31ed6ffd896
Grant Rodgers <grantr@gmail.com>
2012-09-19 11:35:31 -0700

    report incidents to a logfile in tests
    Also terminate the default reporter to silence messages to stderr.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,10 @@ require 'bundler/setup'
 require 'celluloid'
 require 'celluloid/rspec'
 
-# logfile = File.open(File.expand_path("../../log/test.log", __FILE__), 'a')
-# Celluloid.logger = Logger.new(logfile)
+# Terminate the default incident reporter and replace it with one that logs to a file
+logfile = File.open(File.expand_path("../../log/test.log", __FILE__), 'a')
+Celluloid::Actor[:default_incident_reporter].terminate
+Celluloid::IncidentReporter.supervise_as :test_incident_reporter, logfile
 
 Dir['./spec/support/*.rb'].map {|f| require f }
 
