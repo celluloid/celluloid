2d2a363ffcca5ff256c605fff569ddce64e45027
Grant Rodgers <grantr@gmail.com>
2012-09-24 10:37:34 -0700

    refactor IncidentReporter a bit
    Move silencing and the logging formatter so they can be shared by other log
    consumers.

--- lib/celluloid/logging.rb
+++ lib/celluloid/logging.rb
@@ -2,4 +2,8 @@ require 'celluloid/logging/log_event'
 require 'celluloid/logging/incident'
 require 'celluloid/logging/ring_buffer'
 require 'celluloid/logging/incident_logger'
+
+require 'logger'
+require 'celluloid/logging/silenced_logger'
+require 'celluloid/logging/log_event_formatter'
 require 'celluloid/logging/incident_reporter'
