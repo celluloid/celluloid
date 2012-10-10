b7a3af4875f98f75ec9fde42365e7f08aef80b8a
Tony Arcieri <tony.arcieri@gmail.com>
2012-10-10 12:51:07 -0700

    Disable the IncidentReporter by default
    The IncidentReporter is untested and probably not ready for primetime
    yet. This change disables it by default and uses similar logging to
    0.12.0, which is a stdlib Logger which logs to STDERR.

--- lib/celluloid/boot.rb
+++ lib/celluloid/boot.rb
@@ -2,10 +2,9 @@
 
 # Configure default systemwide settings
 Celluloid.task_class = Celluloid::TaskFiber
+Celluloid.logger     = Logger.new(STDERR)
 
-# Launch the notifications fanout actor
+# Launch default services
 # FIXME: We should set up the supervision hierarchy here
 Celluloid::Notifications::Fanout.supervise_as :notifications_fanout
-
-Celluloid.logger = Celluloid::IncidentLogger.new
 Celluloid::IncidentReporter.supervise_as :default_incident_reporter, STDERR
