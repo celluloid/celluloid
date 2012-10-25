69922df15f0d35e58b97cf874d249755e7bfed7d
Tony Arcieri <tony.arcieri@gmail.com>
2012-10-25 12:11:02 -0700

    Log test output to a logfile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,8 @@ require 'bundler/setup'
 require 'celluloid'
 require 'celluloid/rspec'
 
-# Terminate the default incident reporter and replace it with one that logs to a file
 logfile = File.open(File.expand_path("../../log/test.log", __FILE__), 'a')
-Celluloid::Actor[:default_incident_reporter].silence
-Celluloid::IncidentReporter.supervise_as :test_incident_reporter, logfile
+Celluloid.logger = Logger.new(logfile)
 
 Dir['./spec/support/*.rb'].map {|f| require f }
 
