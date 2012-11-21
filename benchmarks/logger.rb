f2a0994da395b442ecf00c504e6c90206841fb26
Grant Rodgers <grantr@gmail.com>
2012-11-21 12:59:33 -0800

    use a new logger for every scenario

--- benchmarks/logger.rb
+++ benchmarks/logger.rb
@@ -6,32 +6,44 @@ require 'celluloid'
 require 'benchmark/ips'
 require 'logger'
 
-logger = Celluloid::IncidentLogger.new
-
 puts "==== Ruby standard logger ===="
-standard_logger = ::Logger.new("/dev/null")
+standard_logger = ::Logger.new(IO::NULL)
 Benchmark.ips do |ips|
   ips.report("standard")    { standard_logger.error("average error message") }
 end
+standard_logger = nil
+
+puts "==== below level  ===="
+logger = Celluloid::IncidentLogger.new
+Benchmark.ips do |ips|
+  ips.report("below level") { logger.trace("average trace message") }
+end
+logger = nil
 
 puts "==== without consumers ===="
+logger = Celluloid::IncidentLogger.new
 Celluloid::Actor[:default_incident_reporter].terminate
 Benchmark.ips do |ips|
-  ips.report("firehose") { logger.debug("average debug message") }
-  ips.report("incidents")    { logger.error("average error message") }
+  ips.report("firehose w/o consumers")  { logger.debug("average debug message") }
+  ips.report("incidents w/o consumers") { logger.error("average error message") }
 end
+logger = nil
 
 puts "==== with incident reporter ===="
-reporter = Celluloid::IncidentReporter.new("/dev/null")
+logger = Celluloid::IncidentLogger.new
+reporter = Celluloid::IncidentReporter.new(IO::NULL)
 Benchmark.ips do |ips|
-  ips.report("firehose") { logger.debug("average debug message") }
-  ips.report("incidents")    { logger.error("average error message") }
+  ips.report("firehose w/ reporter")  { logger.debug("average debug message") }
+  ips.report("incidents w/ reporter") { logger.error("average error message") }
 end
+logger = nil
+reporter.terminate
 
 puts "==== with incident reporter and firehose consumer ===="
-reporter = Celluloid::IncidentReporter.new("/dev/null")
-consumer = Celluloid::FirehoseConsumer.new("/dev/null")
+logger = Celluloid::IncidentLogger.new
+reporter = Celluloid::IncidentReporter.new(IO::NULL)
+consumer = Celluloid::FirehoseConsumer.new(IO::NULL)
 Benchmark.ips do |ips|
-  ips.report("firehose") { logger.debug("average debug message") }
-  ips.report("incidents")    { logger.error("average error message") }
+  ips.report("firehose w/ reporter/consumer")  { logger.debug("average debug message") }
+  ips.report("incidents w/ reporter/consumer") { logger.error("average error message") }
 end
