3d8dc17b971eb0c5189650ce9a1d2b75ff94dc3f
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-12 22:18:07 -0800

    Update to Celluloid(::IO) 0.13

--- lib/celluloid/zmq/version.rb
+++ lib/celluloid/zmq/version.rb
@@ -1,5 +1,5 @@
 module Celluloid
   module ZMQ
-    VERSION = "0.12.1"
+    VERSION = "0.13.0"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 require 'bundler/setup'
 require 'celluloid/zmq'
+
+logfile = File.open(File.expand_path("../../log/test.log", __FILE__), 'a')
+Celluloid.logger = Logger.new(logfile)
