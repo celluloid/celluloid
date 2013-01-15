43da6dac51c9cb6220a139289f5275cb4325ebe8
Tony Arcieri <tony.arcieri@gmail.com>
2013-01-15 00:17:07 -0800

    Move at_exit handler into boot.rb

--- lib/celluloid.rb
+++ lib/celluloid.rb
@@ -91,19 +91,6 @@ module Celluloid
     end
   end
 
-  # Terminate all actors at exit
-  at_exit do
-    if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
-      # workaround for MRI bug losing exit status in at_exit block
-      # http://bugs.ruby-lang.org/issues/5218
-      exit_status = $!.status if $!.is_a?(SystemExit)
-      shutdown
-      exit exit_status if exit_status
-    else
-      shutdown
-    end
-  end
-
   # Class methods added to classes which include Celluloid
   module ClassMethods
     # Create a new actor
--- lib/celluloid/boot.rb
+++ lib/celluloid/boot.rb
@@ -8,3 +8,16 @@ Celluloid.logger     = Logger.new(STDERR)
 # FIXME: We should set up the supervision hierarchy here
 Celluloid::Notifications::Fanout.supervise_as :notifications_fanout
 Celluloid::IncidentReporter.supervise_as :default_incident_reporter, STDERR
+
+# Terminate all actors at exit
+at_exit do
+  if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
+    # workaround for MRI bug losing exit status in at_exit block
+    # http://bugs.ruby-lang.org/issues/5218
+    exit_status = $!.status if $!.is_a?(SystemExit)
+    Celluloid.shutdown
+    exit exit_status if exit_status
+  else
+    Celluloid.shutdown
+  end
+end
