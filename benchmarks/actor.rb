52186e6cbce09080feba6f9e0488e48d9f460578
Tony Arcieri <tony.arcieri@gmail.com>
2013-01-15 22:07:04 -0800

    Modernize examples
    Fix legacy syntax. Also wrangled into here is an attempt to exert some
    backpressure on the async call benchmark to get a real sense of how
    quickly async calls are being dispatched.
    
    Even with this improvement, variance is still ~18% which indicates
    something screwey is still happening.

--- benchmarks/actor.rb
+++ benchmarks/actor.rb
@@ -7,7 +7,20 @@ require 'benchmark/ips'
 
 class ExampleActor
   include Celluloid
+
+  def initialize
+    @condition = Condition.new
+  end
+
   def example_method; end
+
+  def finished
+    @condition.signal
+  end
+
+  def wait_until_finished
+    @condition.wait
+  end
 end
 
 example_actor = ExampleActor.new
@@ -24,8 +37,17 @@ end
 
 Benchmark.ips do |ips|
   ips.report("spawn")       { ExampleActor.new.terminate }
+  
   ips.report("calls")       { example_actor.example_method }
-  ips.report("async calls") { example_actor.example_method! }
+
+  ips.report("async calls") do |n|
+    waiter = example_actor.future.wait_until_finished
+
+    (n - 1).times { example_actor.async.example_method }
+    example_actor.async.finished
+
+    waiter.value
+  end
 
   ips.report("messages") do |n|
     latch_in << n
