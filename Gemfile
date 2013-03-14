f29c2702d0148d94a7f44e667675c2c172b7a1a5
Tony Arcieri <tony.arcieri@gmail.com>
2013-03-13 23:51:20 -0700

    Coveralls support

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 gemspec
 
+gem 'coveralls', require: false
+
 if RUBY_PLATFORM =~ /darwin/
   gem 'rb-fsevent', '~> 0.9.1'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@ require 'rubygems'
 require 'bundler/setup'
 require 'celluloid'
 require 'celluloid/rspec'
-require 'celluloid/internal_pool'
+require 'coveralls'
+Coveralls.wear!
 
 logfile = File.open(File.expand_path("../../log/test.log", __FILE__), 'a')
 Celluloid.logger = Logger.new(logfile)
