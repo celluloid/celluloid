686d7a650b5be62032ee2f8d0993c8b47cfe4b48
Tim Carey-Smith <tim@spork.in>
2013-04-14 04:17:25 +1200

    Use the NIO response for determining IO

--- lib/celluloid/io/reactor.rb
+++ lib/celluloid/io/reactor.rb
@@ -46,20 +46,17 @@ module Celluloid
 
       # Run the reactor, waiting for events or wakeup signal
       def run_once(timeout = nil)
-        handled = false
-        @selector.select(timeout) do |monitor|
+        handled = @selector.select(timeout) do |monitor|
           task = monitor.value
           monitor.close
 
           if task.running?
-            handled = true
             task.resume
           else
             Logger.warn("reactor attempted to resume a dead task")
           end
         end
-
-        handled
+        handled && handled > 0
       end
     end
   end
