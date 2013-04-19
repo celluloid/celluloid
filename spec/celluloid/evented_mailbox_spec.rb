e35d5ce3034504745d6c9b4e67f8751152fe712c
Tim Carey-Smith <tim@spork.in>
2013-04-19 17:31:43 +1200

    Move IO::Mailbox to EventedMailbox

---
+++ spec/celluloid/evented_mailbox_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+class TestEventedMailbox < Celluloid::EventedMailbox
+  class Reactor
+    def initialize
+      @condition = ConditionVariable.new
+      @mutex = Mutex.new
+    end
+
+    def wakeup
+      @mutex.synchronize do
+        @condition.signal
+      end
+    end
+
+    def run_once(timeout)
+      @mutex.synchronize do
+        @condition.wait(@mutex, timeout)
+      end
+    end
+
+    def shutdown
+    end
+  end
+
+  def initialize
+    super(Reactor)
+  end
+end
+
+describe TestEventedMailbox do
+  it_behaves_like "a Celluloid Mailbox"
+end
