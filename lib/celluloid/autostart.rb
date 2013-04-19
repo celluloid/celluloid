2f9ca290edc798057bfc03afc0d86c52352b0edf
Tim Carey-Smith <tim@spork.in>
2013-04-19 14:41:10 +1200

    Register the at_exit handler by default

--- lib/celluloid/autostart.rb
+++ lib/celluloid/autostart.rb
@@ -1,16 +1,3 @@
 require 'celluloid'
 
 Celluloid.boot
-
-# Terminate all actors at exit
-at_exit do
-  if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
-    # workaround for MRI bug losing exit status in at_exit block
-    # http://bugs.ruby-lang.org/issues/5218
-    exit_status = $!.status if $!.is_a?(SystemExit)
-    Celluloid.shutdown
-    exit exit_status if exit_status
-  else
-    Celluloid.shutdown
-  end
-end
