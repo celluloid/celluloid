acfb55149fe7c8387d7b99453f9091df8e4090ab
Tim Carey-Smith <tim@spork.in>
2013-04-22 02:19:00 +1200

    Add missing delegates for rack hijack in Reel

--- lib/celluloid/io/tcp_socket.rb
+++ lib/celluloid/io/tcp_socket.rb
@@ -7,7 +7,7 @@ module Celluloid
     class TCPSocket < Stream
       extend Forwardable
 
-      def_delegators :@socket, :read_nonblock, :write_nonblock, :close, :closed?
+      def_delegators :@socket, :read_nonblock, :write_nonblock, :close, :close_read, :close_write, :closed?
       def_delegators :@socket, :addr, :peeraddr, :setsockopt
 
       # Open a TCP socket, yielding it to the given block and closing it
