e0b5a639cce34d0968efcc2902202ee3e215807a
Tim Carey-Smith <tim@spork.in>
2013-05-29 15:19:30 -0700

    Reapply custom Thread methods

--- lib/celluloid/thread.rb
+++ lib/celluloid/thread.rb
@@ -5,5 +5,34 @@ module Celluloid
     def celluloid?
       true
     end
+
+    # Obtain the role of this thread
+    def role
+      self[:celluloid_role]
+    end
+
+    def role=(role)
+      self[:celluloid_role] = role
+    end
+
+    # Obtain the Celluloid::Actor object for this thread
+    def actor
+      self[:celluloid_actor]
+    end
+
+    # Obtain the Celluloid task object for this thread
+    def task
+      self[:celluloid_task]
+    end
+
+    # Obtain the Celluloid mailbox for this thread
+    def mailbox
+      self[:celluloid_mailbox]
+    end
+
+    # Obtain the call chain ID for this thread
+    def call_chain_id
+      self[:celluloid_chain_id]
+    end
   end
 end
