9678e290016a2a7f88a1a5d7e1a269e5519bec27
Miles Roberts <mroberts@connextions.com>
2013-08-07 14:27:30 -0400

    probe refactor

--- lib/celluloid.rb
+++ lib/celluloid.rb
@@ -132,6 +132,7 @@ module Celluloid
 
     # Shut down all running actors
     def shutdown
+      $CELLULOID_MONITORING = false if $CELLULOID_MONITORING
       actors = Actor.all
 
       Timeout.timeout(shutdown_timeout) do
--- lib/celluloid/probe.rb
+++ lib/celluloid/probe.rb
@@ -1,72 +1,70 @@
 require 'celluloid'
-
+ 
 $CELLULOID_MONITORING = true
-
+ 
 module Celluloid
   class Probe
     include Celluloid
     include Celluloid::Notifications
     
     NOTIFICATIONS_TOPIC_BASE = 'celluloid.events.%s'
-    
-    def self.queue
-      @queue ||= Queue.new
-    end
-    
-    def self.probe
-      Actor[:probe_actor] or raise DeadActorError, "probe actor not running"
-    end
-    
-    def self.run
-      # spawn the actor if not found
-      unless Actor[:probe_actor] && Actor[:probe_actor].alive?
-        supervise_as(:probe_actor)
+    INITIAL_EVENTS = Queue.new
+ 
+    class << self
+      def run
+        # spawn the actor if not found
+        supervise_as(:probe_actor) unless Actor[:probe_actor] && Actor[:probe_actor].alive?
       end
-      probe.async.run()
-    end
+      
+      def actor_created(actor)
+        trigger_event(:actor_created, actor)
+      end
     
-    def run
-      loop do
-        dispatch_event(*self.class.queue.pop)
+      def actor_named(actor)
+        trigger_event(:actor_named, actor)
+      end
+      
+      def actor_died(actor)
+        trigger_event(:actor_died, actor)
+      end
+      
+      def actors_linked(a, b)
+        a = find_actor(a)
+        b = find_actor(b)
+        trigger_event(:actors_linked, a, b)
+      end
+      
+      private
+      
+      def trigger_event(name, *args)
+        return unless $CELLULOID_MONITORING
+        Actor[:probe_actor].async.dispatch_event(name, args)
+      rescue NoMethodError # thrown if probe actor doesn't exist yet
+        INITIAL_EVENTS << [name, args]
+      end
+    
+      def find_actor(obj)
+        if obj.__send__(:class) == Actor
+          obj
+        elsif owner = obj.instance_variable_get(OWNER_IVAR)
+          owner 
+        end
       end
     end
-
     
-    def self.actor_created(actor)
-      trigger_event(:actor_created, actor)
+    def initialize
+      async.first_run
     end
     
-    def self.actor_named(actor)
-      trigger_event(:actor_named, actor)
+    def first_run
+      until INITIAL_EVENTS.size == 0
+        event = INITIAL_EVENTS.pop
+        dispatch_event(*event)
+      end
     end
-    
-    def self.actor_died(actor)
-      trigger_event(:actor_died, actor)
-    end
-    
-    def self.actors_linked(a, b)
-      a = find_actor(a)
-      b = find_actor(b)
-      trigger_event(:actors_linked, a, b)
-    end
-
-  private
+  
     def dispatch_event(cmd, args)
       publish(NOTIFICATIONS_TOPIC_BASE % cmd, args)
     end
-    
-    def self.trigger_event(name, *args)
-      queue << [name, args]
-    end
-    
-    def self.find_actor(obj)
-      if obj.__send__(:class) == Actor
-        obj
-      elsif owner = obj.instance_variable_get(OWNER_IVAR)
-        owner
-      end
-    end
-    
   end
 end
-
