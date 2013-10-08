85906b0ca5382b9b9eece5549fda1b8abc82de65
Pete Nicholls <pete@metanation.com>
2013-10-08 16:52:10 +1300

    Trap API: handle two signals

--- spec/celluloid/trap_spec.rb
+++ spec/celluloid/trap_spec.rb
@@ -10,16 +10,33 @@ class Bikeshed
   end
 end
 
-describe "Celluloid.trap" do
-  it "handles INFO" do
+describe "Celluloid.trap", actor_system: :global do
+  it "handles two signals" do
     shed = Bikeshed.new
+
+    completed = Struct.new(:value).new(:completed)
+
+    green_future = Celluloid::Future.new
+    red_future = Celluloid::Future.new
 
     Celluloid.trap("INFO") do
       shed.paint("green")
+      green_future.signal completed
     end
 
     Process.kill("INFO", $$)
-    
+
+    green_future.value.should == :completed
     shed.color.should == "green"
+
+    Celluloid.trap("USR2") do
+      shed.paint("red")
+      red_future.signal completed
+    end
+
+    Process.kill("USR2", $$)
+
+    red_future.value.should == :completed
+    shed.color.should == "red"
   end
 end
