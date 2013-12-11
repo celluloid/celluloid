911aa5a4d70fa27ce45ddb73e50bd6a721230829
Tim Carey-Smith <tim@spork.in>
2013-12-10 20:39:43 -0800

    Fix and add specs around supervision groups

--- spec/celluloid/supervision_group_spec.rb
+++ spec/celluloid/supervision_group_spec.rb
@@ -34,36 +34,7 @@ describe Celluloid::SupervisionGroup, actor_system: :global do
     Celluloid::Actor[:example].should be_nil
   end
 
-  context "pool" do
-    before :all do
-      class MyActor
-        attr_reader :args
-        def initialize *args
-          @args = *args
-        end
-      end
-      class MyGroup
-        pool MyActor, :as => :example_pool, :args => 'foo', :size => 3
-      end
-    end
-
-    it "runs applications and passes pool options and actor args" do
-      MyGroup.run!
-      sleep 0.001 # startup time hax
-
-      Celluloid::Actor[:example_pool].should be_running
-      Celluloid::Actor[:example_pool].args.should eq ['foo']
-      Celluloid::Actor[:example_pool].size.should be 3
-    end
-
-    it "allows external access to the internal registry" do
-      supervisor = MyGroup.run!
-
-      supervisor[:example].should be_a MyActor
-    end
-  end
-
-  context "supervision group tree" do
+  context "within nested supervisors" do
     before :all do
       class MyParentGroup < Celluloid::SupervisionGroup
         supervise MyGroup, :as => :my_group
@@ -75,6 +46,52 @@ describe Celluloid::SupervisionGroup, actor_system: :global do
       sleep 0.001
 
       Celluloid::Actor[:example].should be_running
+    end
+  end
+
+  context "pool" do
+    before :all do
+      class MyPoolActor < MyActor
+        attr_reader :args
+        def initialize *args
+          @args = *args
+        end
+      end
+      class MyPoolGroup < Celluloid::SupervisionGroup
+        pool MyPoolActor, :as => :example_pool, :args => 'foo', :size => 3
+      end
+    end
+
+    it "runs applications and passes pool options and actor args" do
+      MyPoolGroup.run!
+      sleep 0.001 # startup time hax
+
+      Celluloid::Actor[:example_pool].should be_running
+      Celluloid::Actor[:example_pool].args.should eq ['foo']
+      Celluloid::Actor[:example_pool].size.should be 3
+    end
+
+    it "allows external access to the internal registry" do
+      supervisor = MyPoolGroup.run!
+
+      supervisor[:example_pool].should be_a MyPoolActor
+    end
+
+    context "within nested supervisors" do
+      before :all do
+        class MyParentPoolGroup < Celluloid::SupervisionGroup
+          pool MyPoolGroup, :as => :my_group
+        end
+      end
+
+      it "starts child-supervised actors" do
+        MyParentPoolGroup.run!
+        sleep 0.001
+
+        Celluloid::Actor[:example_pool].should be_running
+        Celluloid::Actor[:example_pool].args.should eq ['foo']
+        Celluloid::Actor[:example_pool].size.should be 3
+      end
     end
   end
 end
