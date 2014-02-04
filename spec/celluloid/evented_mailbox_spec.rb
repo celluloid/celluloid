3649f204d2726348e295e350f437e879545a42ca
Tiago Cardoso <tiago.cardoso@swisscom.com>
2014-02-04 13:14:00 +0100

    added spec which specifies the issue of reactor crashing when Kernel.timeout blows (this spec fails)

--- spec/celluloid/evented_mailbox_spec.rb
+++ spec/celluloid/evented_mailbox_spec.rb
@@ -31,4 +31,15 @@ end
 describe Celluloid::EventedMailbox do
   subject { TestEventedMailbox.new }
   it_behaves_like "a Celluloid Mailbox"
+  it "recovers from timeout exceeded to process mailbox message" do
+    timeout_interval = Celluloid::TIMER_QUANTUM + 0.1
+    expect do
+      Kernel.send(:timeout, timeout_interval) do
+        subject.receive { false }
+      end
+    end.to raise_exception(Celluloid::TimeoutError)
+
+    (Time.now - started_at).should be_within(Celluloid::TIMER_QUANTUM).of interval
+  end
+
 end
