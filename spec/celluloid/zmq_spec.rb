3c416f1d922c487f8a0e86824943fafd55f4fa92
Jonas Nicklas <jonas.nicklas@gmail.com>
2014-02-04 09:54:38 +0100

    Use inproc instead of TCP, so we don't have to mess around with open ports

--- spec/celluloid/zmq_spec.rb
+++ spec/celluloid/zmq_spec.rb
@@ -1,28 +1,17 @@
 require 'spec_helper'
 
-# find some available ports for ZMQ
-ZMQ_PORTS = 10.times.map do
-  begin
-    server = TCPServer.new('127.0.0.1', 0)
-    server.addr[1]
-  ensure
-    server.close if server
-  end
-end
-
 describe Celluloid::ZMQ do
   before { @sockets = [] }
   after { @sockets.each(&:close) }
-  let(:ports) { ZMQ_PORTS }
 
   def connect(socket, index=0)
-    socket.connect("tcp://127.0.0.1:#{ports[index]}")
+    socket.connect("inproc://celluloid-spec-#{index}")
     @sockets << socket
     socket
   end
 
   def bind(socket, index=0)
-    socket.bind("tcp://127.0.0.1:#{ports[index]}")
+    socket.bind("inproc://celluloid-spec-#{index}")
     @sockets << socket
     socket
   end
@@ -68,9 +57,9 @@ describe Celluloid::ZMQ do
 
         finalizer :close_socket
 
-        def initialize(port)
+        def initialize(index)
           @socket = Celluloid::ZMQ::RepSocket.new
-          @socket.connect("tcp://127.0.0.1:#{port}")
+          @socket.connect("inproc://celluloid-spec-#{index}")
         end
 
         def say_hi
@@ -89,7 +78,7 @@ describe Celluloid::ZMQ do
 
     it "receives messages" do
       server = bind(Celluloid::ZMQ.context.socket(::ZMQ::REQ))
-      client = actor.new(ports[0])
+      client = actor.new(0)
 
       server.send_string("hello world")
       result = client.fetch
@@ -98,7 +87,7 @@ describe Celluloid::ZMQ do
 
     it "suspends actor while waiting for message" do
       server = bind(Celluloid::ZMQ.context.socket(::ZMQ::REQ))
-      client = actor.new(ports[0])
+      client = actor.new(0)
 
       result = client.future.fetch
       client.say_hi.should eq("Hi!")
@@ -114,9 +103,9 @@ describe Celluloid::ZMQ do
 
         finalizer :close_socket
 
-        def initialize(port)
+        def initialize(index)
           @socket = Celluloid::ZMQ::ReqSocket.new
-          @socket.connect("tcp://127.0.0.1:#{port}")
+          @socket.connect("inproc://celluloid-spec-#{index}")
         end
 
         def say_hi
@@ -136,7 +125,7 @@ describe Celluloid::ZMQ do
 
     it "sends messages" do
       client = bind(Celluloid::ZMQ.context.socket(::ZMQ::REP))
-      server = actor.new(ports[0])
+      server = actor.new(0)
 
       server.send("hello world")
 
@@ -147,7 +136,7 @@ describe Celluloid::ZMQ do
 
     it "suspends actor while waiting for message to be sent" do
       client = bind(Celluloid::ZMQ.context.socket(::ZMQ::REP))
-      server = actor.new(ports[0])
+      server = actor.new(0)
 
       result = server.future.send("hello world")
 
