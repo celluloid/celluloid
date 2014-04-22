42b218fc89128e2ecbdb90cec2887fbba4681ce6
Dave Copeland <davec@naildrivin5.com>
2014-04-22 09:36:15 -0400

    lazily load the number of cores + test coverage
    Since this was happening when the class was loaded, it was impossible to
    test.  Also, problems during this load can bring down an app during
    startup, which is not desired.

--- lib/celluloid/cpu_counter.rb
+++ lib/celluloid/cpu_counter.rb
@@ -2,24 +2,31 @@ require 'rbconfig'
 
 module Celluloid
   module CPUCounter
-    case RbConfig::CONFIG['host_os'][/^[A-Za-z]+/]
-    when 'darwin'
-      @cores = Integer(`/usr/sbin/sysctl hw.ncpu`[/\d+/])
-    when 'linux'
-      @cores = if File.exists?("/sys/devices/system/cpu/present")
-        File.read("/sys/devices/system/cpu/present").split('-').last.to_i+1
-      else
-        Dir["/sys/devices/system/cpu/cpu*"].select { |n| n=~/cpu\d+/ }.count
-      end
-    when 'mingw', 'mswin'
-      @cores = Integer(ENV["NUMBER_OF_PROCESSORS"][/\d+/])
-    when 'freebsd'
-      @cores = Integer(`sysctl hw.ncpu`[/\d+/])
-    else
-      @cores = nil
+    def self.cores
+      @cores ||= count_cores
     end
 
-    def self.cores; @cores; end
+  private
+
+    def self.count_cores
+      case RbConfig::CONFIG['host_os'][/^[A-Za-z]+/]
+      when 'darwin'
+        @cores = Integer(`/usr/sbin/sysctl hw.ncpu`[/\d+/])
+      when 'linux'
+        @cores = if File.exists?("/sys/devices/system/cpu/present")
+                   File.read("/sys/devices/system/cpu/present").split('-').last.to_i+1
+                 else
+                   Dir["/sys/devices/system/cpu/cpu*"].select { |n| n=~/cpu\d+/ }.count
+                 end
+      when 'mingw', 'mswin'
+        @cores = Integer(ENV["NUMBER_OF_PROCESSORS"][/\d+/])
+      when 'freebsd'
+        @cores = Integer(`sysctl hw.ncpu`[/\d+/])
+      else
+        @cores = nil
+      end
+    end
+
   end
 end
 
--- spec/celluloid/cpu_counter_spec.rb
+++ spec/celluloid/cpu_counter_spec.rb
@@ -2,8 +2,67 @@ require 'spec_helper'
 
 describe Celluloid::CPUCounter do
   describe :cores do
-    it 'should return an integer' do
-      Celluloid::CPUCounter.cores.should be_kind_of(Fixnum)
+    before do
+      @actual_host_os = RbConfig::CONFIG['host_os']
+      RbConfig::CONFIG['host_os'] = fake_host_os
+      Celluloid::CPUCounter.instance_variable_set("@cores",nil)
+    end
+    after do
+      RbConfig::CONFIG['host_os'] = @actual_host_os
+      ENV["NUMBER_OF_PROCESSORS"] = nil
+    end
+    let(:num_cores) { 1024 }
+    context 'darwin' do
+      let(:fake_host_os) { 'darwin' }
+      it "uses sysctl" do
+        Celluloid::CPUCounter.should_receive(:`).with("/usr/sbin/sysctl hw.ncpu").and_return("hw.ncpu: #{num_cores}")
+        Celluloid::CPUCounter.cores.should == num_cores
+      end
+    end
+    context 'linux' do
+      let(:fake_host_os) { 'linux' }
+      context "when /sys/devices/system/cpu/present exists" do
+        it "reads CPU info from there" do
+          File.should_receive(:exists?).with("/sys/devices/system/cpu/present").and_return(true)
+          File.should_receive(:read).with("/sys/devices/system/cpu/present").and_return("dunno-whatever-#{num_cores - 1}")
+          Celluloid::CPUCounter.cores.should == num_cores
+        end
+      end
+      context "when /sys/devices/system/cpu/present does NOT exists" do
+        it "counts the number of cpu entries in /sys/devices/system/cpu/" do
+          File.should_receive(:exists?).with("/sys/devices/system/cpu/present").and_return(false)
+          cpu_entries = (1..num_cores).map { |n| "cpu#{n}" } + ["non-cpu-entry-to-ignore"]
+          Dir.should_receive(:[]).with("/sys/devices/system/cpu/cpu*").and_return(cpu_entries)
+          Celluloid::CPUCounter.cores.should == num_cores
+        end
+      end
+    end
+    context 'mingw' do
+      let(:fake_host_os) { 'mingw' }
+      it "uses the environment" do
+        ENV["NUMBER_OF_PROCESSORS"] = num_cores.to_s
+        Celluloid::CPUCounter.cores.should == num_cores
+      end
+    end
+    context 'mswin' do
+      let(:fake_host_os) { 'mswin' }
+      it "uses the environment" do
+        ENV["NUMBER_OF_PROCESSORS"] = num_cores.to_s
+        Celluloid::CPUCounter.cores.should == num_cores
+      end
+    end
+    context 'freebsd' do
+      let(:fake_host_os) { 'freebsd' }
+      it "uses sysctl" do
+        Celluloid::CPUCounter.should_receive(:`).with("sysctl hw.ncpu").and_return("hw.ncpu: #{num_cores}")
+        Celluloid::CPUCounter.cores.should == num_cores
+      end
+    end
+    context 'ENCOM OS-12' do
+      let(:fake_host_os) { 'encom_os-12' }
+      it "has no clue, so just sets it to nil" do
+        Celluloid::CPUCounter.cores.should == nil
+      end
     end
   end
 end
