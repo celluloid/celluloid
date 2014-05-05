7fdef04c13a6e43e8f73ea544e9b1de909c198d0
Tony Arcieri <bascule@gmail.com>
2014-05-05 01:56:09 -0700

    CPU counting fixups

--- lib/celluloid/cpu_counter.rb
+++ lib/celluloid/cpu_counter.rb
@@ -9,19 +9,24 @@ module Celluloid
   private
 
     def self.count_cores
-      result = ENV['NUMBER_OF_PROCESSORS'] || case RbConfig::CONFIG['host_os'][/^[A-Za-z]+/]
-      when 'darwin'
-        `/usr/sbin/sysctl -n hw.ncpu`
-      when /bsd|dragonfly/
-        `/sbin/sysctl -n hw.ncpu`
-      when 'linux'
-        begin
-          return ::IO.read('/sys/devices/system/cpu/present').split('-').last.to_i+1
-        rescue Errno::ENOENT
-          return Dir["/sys/devices/system/cpu/cpu*"].select { |n| n=~/cpu\d+/ }.count
+      result = ENV['NUMBER_OF_PROCESSORS']
+      return Integer(result, 10) if result
+
+      result = 
+        case RbConfig::CONFIG['host_os'][/^[A-Za-z]+/]
+        when 'darwin'
+          `/usr/sbin/sysctl -n hw.ncpu`
+        when /bsd|dragonfly/
+          `/sbin/sysctl -n hw.ncpu`
+        when 'linux'
+          begin
+            return ::IO.read('/sys/devices/system/cpu/present').split('-').last.to_i+1
+          rescue Errno::ENOENT
+            return Dir["/sys/devices/system/cpu/cpu*"].select { |n| n=~/cpu\d+/ }.count
+          end
         end
-      end
-      result && Integer(result[/\d+/])
+
+      result && Integer(result.to_s[/\d+/], 10)
     end
   end
 end
