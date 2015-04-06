9726a4cd3f108cbd93f1dbbb295bceac58a320a3
//de <de@freed.network>
2015-04-06 08:29:03 -0700

    closes #382

--- spec/celluloid/evented_mailbox_spec.rb
+++ spec/celluloid/evented_mailbox_spec.rb
@@ -29,4 +29,16 @@ end
 RSpec.describe Celluloid::EventedMailbox do
   subject { TestEventedMailbox.new }
   it_behaves_like "a Celluloid Mailbox"
+
+  it "recovers from timeout exceeded to process mailbox message" do
+    timeout_interval = CelluloidSpecs::TIMER_QUANTUM + 0.1
+    started_at = Time.now
+    expect do
+      Kernel.send(:timeout, timeout_interval) do
+        subject.receive { false }
+      end
+    end.to raise_exception(Timeout::Error)
+
+    expect(Time.now - started_at).to be_within(CelluloidSpecs::TIMER_QUANTUM).of timeout_interval
+  end
 end
