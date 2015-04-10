447c412324b30df7e188bb07047fa67e239d8c54
Cezary Baginski <cezary@chronomantic.net>
2015-04-10 17:04:42 +0200

    avoid filtering specs on Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,7 +111,10 @@ Celluloid.shutdown_timeout = 1
 Dir['./spec/support/*.rb'].map {|f| require f }
 
 RSpec.configure do |config|
-  config.filter_run :focus => true
+  unless Nenv.ci?
+    config.filter_run :focus => true
+  end
+
   config.run_all_when_everything_filtered = true
   config.disable_monkey_patching!
   config.profile_examples = 3
