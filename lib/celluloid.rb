07308818495f6589e00840a78b42605de51e0b08
//de <de@freed.network>
2015-04-13 15:49:08 -0700

    Remove supervision related items, move to gem.

--- lib/celluloid.rb
+++ lib/celluloid.rb
@@ -188,18 +188,6 @@ module Celluloid
     end
     alias_method :spawn_link, :new_link
 
-    # Create a supervisor which ensures an instance of an actor will restart
-    # an actor if it fails
-    def supervise(*args, &block)
-      Supervisor.supervise(self, *args, &block)
-    end
-
-    # Create a supervisor which ensures an instance of an actor will restart
-    # an actor if it fails, and keep the actor registered under a given name
-    def supervise_as(name, *args, &block)
-      Supervisor.supervise_as(name, self, *args, &block)
-    end
-
     # Run an actor in the foreground
     def run(*args, &block)
       Actor.join(new(*args, &block))
