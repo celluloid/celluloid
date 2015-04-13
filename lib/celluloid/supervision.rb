05555345e2d4269d4d0d04e877775fa52f22814c
//de <de@freed.network>
2015-04-13 13:11:47 -0700

    bring over Class Methods on actors.

--- lib/celluloid/supervision.rb
+++ lib/celluloid/supervision.rb
@@ -2,3 +2,21 @@ require 'celluloid'
 
 require 'celluloid/supervision_group'
 require 'celluloid/supervisor'
+
+module Celluloid
+  module ClassMethods
+
+    # Create a supervisor which ensures an instance of an actor will restart
+    # an actor if it fails
+    def supervise(*args, &block)
+      Supervisor.supervise(self, *args, &block)
+    end
+
+    # Create a supervisor which ensures an instance of an actor will restart
+    # an actor if it fails, and keep the actor registered under a given name
+    def supervise_as(name, *args, &block)
+      Supervisor.supervise_as(name, self, *args, &block)
+    end
+
+  end
+end
