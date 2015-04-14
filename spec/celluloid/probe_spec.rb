155e7cbd76b408a3372dc1d61886f563a856facd
Cezary Baginski <cezary@chronomantic.net>
2015-04-14 19:45:10 +0200

    fix occasionally failing Probe + specs
    Probe uses Actor[:probe_actor] and this wasn't set,
    so Probe only handled the events that were recorded
    before initialize/first_run.
    
    Now Probe will keep collecting and processing events even if it's
    no longer in the registry.

--- spec/celluloid/probe_spec.rb
+++ spec/celluloid/probe_spec.rb
@@ -58,12 +58,13 @@ RSpec.describe "Probe", actor_system: :global do
   describe 'on boot' do
     it 'should capture system actor spawn', flaky: true do
       TestProbeClient.new(queue)
-      Celluloid::Actor[:probe] = Celluloid::Probe.new
       create_events = []
       received_named_events = {
         :default_incident_reporter => nil,
         :notifications_fanout      => nil
       }
+
+      Celluloid::Probe.run_without_supervision
       Timeout.timeout(5) do
         loop do
           ev = queue.pop
@@ -77,6 +78,7 @@ RSpec.describe "Probe", actor_system: :global do
           break if received_named_events.all?{|_, v| v != nil }
         end
       end
+
       expect(received_named_events.all?{|_, v| v != nil }).to eq(true)
       # now check we got the create events for every actors
       received_named_events.each do |_, mailbox_address|
@@ -93,34 +95,41 @@ RSpec.describe "Probe", actor_system: :global do
   describe 'after boot' do
     it 'should send a notification when an actor is spawned', flaky: true do
       TestProbeClient.new(queue)
-      Celluloid::Actor[:probe] = Celluloid::Probe.new
       a = DummyActor.new
+
+      Celluloid::Probe.run_without_supervision
       expect(wait_for_match(queue, 'celluloid.events.actor_created', a)).to be
     end
 
     it 'should send a notification when an actor is named' do
       TestProbeClient.new(queue)
-      Celluloid::Actor[:probe] = Celluloid::Probe.new
       a = DummyActor.new
       Celluloid::Actor['a name'] = a
+      Specs.sleep_and_wait_until { Celluloid::Actor['a name'] == a }
+
+      Celluloid::Probe.run_without_supervision
       expect(wait_for_match(queue, 'celluloid.events.actor_named', a)).to be
     end
 
     it 'should send a notification when actor dies'  do
       TestProbeClient.new(queue)
-      Celluloid::Actor[:probe] = Celluloid::Probe.new
       a = DummyActor.new
       a.terminate
+      Specs.sleep_and_wait_until { !a.alive? }
+
+      Celluloid::Probe.run_without_supervision
       expect(wait_for_match(queue, 'celluloid.events.actor_died', a)).to be
     end
 
     it 'should send a notification when actors are linked' do
       TestProbeClient.new(queue)
-      Celluloid::Actor[:probe] = Celluloid::Probe.new
       a = DummyActor.new
       b = DummyActor.new
       a.link(b)
 
+      Specs.sleep_and_wait_until { a.linked_to?(b) }
+
+      Celluloid::Probe.run_without_supervision
       expect(wait_for_match(queue, 'celluloid.events.actors_linked', a, b)).to be
     end
   end
