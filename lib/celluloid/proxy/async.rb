b6c0c1907c6f60ed7aef7738127f8e17ee6170ff
//de <de@freed.network>
2015-04-14 19:16:07 -0700

    class naming refactor, per convention: #577

---
+++ lib/celluloid/proxy/async.rb
@@ -0,0 +1,31 @@
+module Celluloid::Proxy
+  # A proxy which sends asynchronous calls to an actor
+  class Async < Proxy::Abstract
+    attr_reader :mailbox
+
+    # Used for reflecting on proxy objects themselves
+    def __class__; Proxy::Async; end
+
+    def initialize(mailbox, klass)
+      @mailbox, @klass = mailbox, klass
+    end
+
+    def inspect
+      "#<Celluloid::Proxy::Async(#{@klass})>"
+    end
+
+    def method_missing(meth, *args, &block)
+      if @mailbox == ::Thread.current[:celluloid_mailbox]
+        args.unshift meth
+        meth = :__send__
+      end
+
+      if block_given?
+        # FIXME: nicer exception
+        raise "Cannot use blocks with async yet"
+      end
+
+      @mailbox << AsyncCall.new(meth, args, block)
+    end
+  end
+end
