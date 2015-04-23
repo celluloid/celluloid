33771b2634f6690e1cc1e7c183fe83f88d26dcec
//de <de@freed.network>
2015-04-22 17:39:32 -0700

    contents of depreciate.rb

--- lib/celluloid/depreciate.rb
+++ lib/celluloid/depreciate.rb
@@ -1,3 +1,13 @@
+class Thread
+  def self.mailbox
+    Celluloid.mailbox
+  end
+
+  def self.receive(timeout = nil, &block)
+    Celluloid.receive(timeout, &block)
+  end
+end
+
 # TODO: Remove link to Interal::Logger
 module Celluloid
   Logger = Internals::Logger
