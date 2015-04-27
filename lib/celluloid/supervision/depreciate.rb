b705b4e915e968520678c1f76f9411da0ca64ce3
//de <de@freed.network>
2015-04-26 23:42:08 -0700

    final English fix

--- lib/celluloid/supervision/depreciate.rb
+++
@@ -1,8 +0,0 @@
-# TODO: Remove at 1.0
-module Celluloid
-  SupervisionGroup = Supervision::Container
-end
-
-require 'celluloid/supervision/deprecate/supervision'
-require 'celluloid/supervision/deprecate/supervisor'
-require 'celluloid/supervision/deprecate/configuration'
