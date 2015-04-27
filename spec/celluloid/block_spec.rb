af5bc6fbc76b8de7fc3f92a982f6c0a5e3efd0f5
Kim Burgestrand <kim@burgestrand.se>
2015-04-27 08:41:51 +0200

    Remove use of globals in block spec suite.

--- spec/celluloid/block_spec.rb
+++ spec/celluloid/block_spec.rb
@@ -7,21 +7,22 @@ RSpec.describe "Blocks", actor_system: :global do
     end
     attr_reader :name
 
-    def ask_for_something(other)
+    def ask_for_something(other, trace = [])
       sender_actor = current_actor
-      $data << [:outside, @name, current_actor.name]
-      other.do_something_and_callback do |value|
-        $data << [:yielded, @name, current_actor.name]
-        $data << receive_result(:self)
-        $data << current_actor.receive_result(:current_actor)
-        $data << sender_actor.receive_result(:sender)
+      trace << [:outside, @name, current_actor.name]
+      other.do_something_and_callback(trace) do |value|
+        trace << [:yielded, @name, current_actor.name]
+        trace << self.receive_result(:self)
+        trace << current_actor.receive_result(:current_actor)
+        trace << sender_actor.receive_result(:sender)
         "somevalue"
       end
+      trace
     end
 
-    def do_something_and_callback
-      $data << [:something, @name, current_actor.name]
-      $data << yield(:foo)
+    def do_something_and_callback(trace)
+      trace << [:something, @name, current_actor.name]
+      trace << yield(:foo)
     end
 
     def receive_result(result)
@@ -29,15 +30,12 @@ RSpec.describe "Blocks", actor_system: :global do
     end
   end
 
-  it "works" do
-    $data = []
-
+  it "executes blocks on sender" do
     a1 = MyBlockActor.new("one")
     a2 = MyBlockActor.new("two")
 
-    a1.ask_for_something a2
-
-    expected = [
+    trace = a1.ask_for_something a2
+    expect(trace).to eq([
       [:outside, "one", "one"],
       [:something, "two", "two"],
       [:yielded, "one", "one"],
@@ -45,8 +43,6 @@ RSpec.describe "Blocks", actor_system: :global do
       [:current_actor, "one", "one"],
       [:sender, "one", "one"],
       "somevalue",
-    ]
-
-    expect($data).to eq(expected)
+    ])
   end
 end
