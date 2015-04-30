4e06f6e717137b31c9972ac163b0e017384d65af
//de <de@freed.network>
2015-04-29 20:48:53 -0700

    update version message, and location

--- lib/celluloid/notices.rb
+++ lib/celluloid/notices.rb
@@ -4,17 +4,23 @@ module Celluloid
       @@notices = []
 
       def backported_mini
-        @@notices << [:warn, "Celluloid is running in BACKPORTED mode. [ http://git.io/vfteb ]"]
+        @@notices << [:warn, "Celluloid is running in BACKPORTED mode. [ http://git.io/vJf3J ]"]
+      end
+
+      def version
+        @@notices << [:info, "+--------------------------------------------------+"]
+        @@notices << [:info, "|     Celluloid version running now: #{"%-9s" %Celluloid::VERSION}      +"]
       end
 
       def backported
+        version
         @@notices << [:warn, "+--------------------------------------------------+"]
         @@notices << [:warn, "|     Celluloid is running in BACKPORTED mode.     |"]
         @@notices << [:warn, "|   Time to update deprecated code, before v1.0!   |"]
         @@notices << [:warn, "+--------------------------------------------------+"]
         @@notices << [:warn, "|  Prepare! As of v0.17.5 you can begin updating.  |"]
         @@notices << [:warn, "+--------------------------------------------------+"]
-        @@notices << [:warn, "|    Want to read about it? http://git.io/vfteb    |"]
+        @@notices << [:warn, "|    Want to read about it? http://git.io/vJf3J    |"]
         @@notices << [:warn, "+--------------------------------------------------+"]
       end
 
