4a3584bc9890aab44c367768db5a896a125d5b20
//de <de@freed.network>
2015-05-06 05:15:08 -0700

    slight reorganization of method location, and introduction of router

--- lib/celluloid/supervision.rb
+++ lib/celluloid/supervision.rb
@@ -1,6 +1,7 @@
 require "celluloid" unless defined? Celluloid
 
 require "celluloid/supervision/constants"
+require "celluloid/supervision/supervise"
 
 require "celluloid/supervision/container"
 require "celluloid/supervision/container/instance"
@@ -13,16 +14,5 @@ require "celluloid/supervision/configuration"
 require "celluloid/supervision/configuration/instance"
 
 require "celluloid/supervision/service"
-
-module Celluloid
-  module ClassMethods
-    def supervise(config={}, &block)
-      Celluloid.services.supervise(config.merge(type: self, block: block))
-    rescue NoMethodError
-      Internals::Logger.warn("No public supervision service was found. Supervising #{self} a la carte.")
-      Supervision::Container.supervise(config.merge(type: self, block: block))
-    end
-  end
-end
 
 require "celluloid/supervision/deprecate" unless $CELLULOID_BACKPORTED == false
---
+++ lib/celluloid/supervision/supervise.rb
@@ -0,0 +1,33 @@
+# collect together all instances of the `supervise` method
+module Celluloid
+  class << self
+    def supervise(config={}, &block)
+      # select which supervisor, and route this call to that directly.
+      # for now, use .services
+      services.supervise(config, &block)
+    end
+  end
+  module ClassMethods
+    def supervise(config={}, &block)
+      Celluloid.supervise(config.merge(type: self, block: block))
+    rescue NoMethodError
+      Internals::Logger.warn("No public supervision service was found. Supervising #{self} a la carte.")
+      Supervision::Container.supervise(config.merge(type: self, block: block))
+    end
+  end
+  module Supervision
+    class Container
+      class << self
+        def supervise(config, &block)
+          blocks << lambda do |container|
+            container.add(Configuration.options(config, block: block))
+          end
+        end
+      end
+
+      def supervise(config, &block)
+        add(Configuration.options(config, block: block))
+      end
+    end
+  end
+end
