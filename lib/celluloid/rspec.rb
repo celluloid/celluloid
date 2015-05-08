77d3a09b832f7c2e49b2f566107009c0fc76ed76
//de <de@freed.network>
2015-05-07 23:25:46 -0700

    bring shared examples into scope for other gems

--- lib/celluloid/rspec.rb
+++ lib/celluloid/rspec.rb
@@ -1,3 +1,6 @@
 require "celluloid/test"
 
 $CELLULOID_DEBUG = true
+
+# Load shared examples for other gems to use.
+Dir["#{File.expand_path("../../../spec/shared", __FILE__)}/*.rb"].map {|f| require f }
