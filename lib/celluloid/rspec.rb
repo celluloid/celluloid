5b63362c1bbecbfe2614fc0d08d6389058c58f0c
//de <de@freed.network>
2015-05-07 23:43:13 -0700

    add support/* requirements to be used outside Celluloid itself

--- lib/celluloid/rspec.rb
+++ lib/celluloid/rspec.rb
@@ -2,5 +2,19 @@ require "celluloid/test"
 
 $CELLULOID_DEBUG = true
 
-# Load shared examples for other gems to use.
+# Load shared examples and test support code for other gems to use.
+
+[
+  "env",
+  "logging",
+  "split_logs",
+  "sleep_and_wait",
+  "reset_class_variables",
+  "crash_checking",
+  "stubbing",
+  "coverage"
+].each { |f|
+  require "#{File.expand_path("../../../spec/support", __FILE__)}/#{f}.rb"
+}
+
 Dir["#{File.expand_path("../../../spec/shared", __FILE__)}/*.rb"].map {|f| require f }
