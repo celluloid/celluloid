edf96cff451c50fe3282207fb31ccd28255561f9
//de <code@extremist.digital>
2015-06-18 21:31:08 -0700

    Spec changes.

--- spec/shared/actor_examples.rb
+++ spec/shared/actor_examples.rb
@@ -382,8 +382,9 @@ RSpec.shared_examples "a Celluloid Actor" do
     end
 
     it "allows mocking async calls via the async proxy" do
-      pending "Fails due to RSpec's new expectation verification"
-      fail "TODO: may never work in newer versions of RSpec (no method on Proxy::Async)"
+      # TODO: Verify via CIA... This appears to be working, with new celluloid/rspec handler.
+      # pending "Fails due to RSpec's new expectation verification"
+      # fail "TODO: may never work in newer versions of RSpec (no method on Proxy::Async)"
       expect(actor.async).to receive(:greet).once
       actor.async.greet
     end
