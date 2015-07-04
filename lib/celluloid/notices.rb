3dec29be8c5afe9876d77f422f04d8bdf7ee4ed1
//de <code@extremist.digital>
2015-07-04 01:22:27 -0700

    use only the mini message, make it a DEBUG message... step up to WARN at later release #641

--- lib/celluloid/notices.rb
+++ lib/celluloid/notices.rb
@@ -3,25 +3,8 @@ module Celluloid
     class << self
       @@notices = []
 
-      def backported_mini
-        @@notices << [:warn, "Celluloid is running in BACKPORTED mode. [ http://git.io/vJf3J ]"]
-      end
-
-      def version
-        @@notices << [:info, "+--------------------------------------------------+"]
-        @@notices << [:info, "|    Celluloid version running now: #{'%-13s' % Celluloid::VERSION}  +"]
-      end
-
       def backported
-        version
-        @@notices << [:warn, "+--------------------------------------------------+"]
-        @@notices << [:warn, "|     Celluloid is running in BACKPORTED mode.     |"]
-        @@notices << [:warn, "|   Time to update deprecated code, before v1.0!   |"]
-        @@notices << [:warn, "+--------------------------------------------------+"]
-        @@notices << [:warn, "|  Prepare! As of v0.17.5 you can begin updating.  |"]
-        @@notices << [:warn, "+--------------------------------------------------+"]
-        @@notices << [:warn, "|    Want to read about it? http://git.io/vJf3J    |"]
-        @@notices << [:warn, "+--------------------------------------------------+"]
+        @@notices << [:debug, "Celluloid #{Celluloid::VERSION} is running in BACKPORTED mode. [ http://git.io/vJf3J ]"]
       end
 
       def output
