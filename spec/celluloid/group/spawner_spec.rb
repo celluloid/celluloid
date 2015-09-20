5725feeec7e78e83298daacb670a85f20971f3ff
Vladimir Kochnev <hashtable@yandex.ru>
2015-09-20 15:13:29 +0300

    Clean finished threads in Group::Spawner.

--- spec/celluloid/group/spawner_spec.rb
+++ spec/celluloid/group/spawner_spec.rb
@@ -4,5 +4,12 @@ require "celluloid/internals/logger"
 if Celluloid.group_class == Celluloid::Group::Spawner
   RSpec.describe Celluloid::Group::Spawner do
     it_behaves_like "a Celluloid Group"
+
+    it "does not leak finished threads" do
+      th = subject.get { sleep 0.1 }
+      expect {
+        th.join
+      }.to change{ subject.group.length }.by(-1)
+    end
   end
 end
