9fbb7b9cbb54743aa46fc9f6bb1f0a308bed585d
//de <code@extremist.digital>
2016-06-09 03:36:15 -0700

    start of multiplex

---
+++ spec/celluloid/multiplex_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe Celluloid::Multiplex, actor_system: :global do
+
+  context "resolving Futures" do
+    it "should fire off the resolution block as each Future becomes available" do
+      fail NotImplementedError
+    end
+  end
+
+  context "resolving Conditions" do
+    it "should fire off the resolution block as each Condition becomes available" do
+      fail NotImplementedError
+    end
+  end
+end
