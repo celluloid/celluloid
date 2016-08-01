792b50c500e01fc475da2cdb1aae2676b11d3851
Aayush Ranaut <aayush.ranaut@gmail.com>
2016-08-01 16:53:04 +0530

    Added results for ring benchmark

--- benchmarks/ring.rb
+++ benchmarks/ring.rb
@@ -12,3 +12,8 @@ ring = Ring.new 512
 Benchmark.ips do |ips|
   ips.report("ring-around") { |n| ring.run n }
 end
+
+# Calculating -------------------------------------
+# ring-around         1 i/100ms
+# -------------------------------------------------
+# ring-around       19.7 (±25.4%) i/s -         92 in   5.005323s
